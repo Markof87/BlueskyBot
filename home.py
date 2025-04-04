8994b82c5482b743eec3da9900d0f63b8c68f033
fabianim <m.fabiani@ns12.it>
2025-04-04 17:05:57 +0200

    HO SVOLTATO!

--- home.py
+++ home.py
@@ -6,6 +6,37 @@ import numpy as np
 import plotly.express as px
 
 import config
+import utils
+import reports
+import webbrowser
+from io import BytesIO
+from PIL import Image
+
+def image_creator(url, event_name, name, opponent):
+
+    response = requests.get(url)
+    # Assicurati che la richiesta sia andata a buon fine
+    if response.status_code == 200:
+        match_data = response.json()
+    else:
+        print(f"Errore nella richiesta: {response.status_code}")
+
+    img_buffer = reports.getEventReport(match_data, event_name, name, opponent, pitch_color='#FFFFFF')
+    img_buffer.seek(0)
+    image_data = utils.compress_image(img_buffer.read(), target_size_kb=976.56, initial_resize_factor=1.0)
+
+    image = Image.open(BytesIO(image_data))
+    image.show()
+
+    with open('report_image.png', 'wb') as f:
+        f.write(image_data)
+    print("Immagine salvata come debug_image.png per verifica.")
+
+    if not image_data:
+        print("Errore: Il buffer dell'immagine è vuoto.")
+        exit(1)
+
+    #upload = client.com.atproto.repo.upload_blob(image_data, timeout=120)
 
 home_url = config.HOME_URL
 
@@ -100,8 +131,6 @@ def add_css():
 # Aggiungere il CSS custom per lo stile cyberpunk
 #add_css()
 
-st.title("⚽ Partite del giorno")
-
 url = home_url + 'tournaments'
 # Fai la richiesta GET per ottenere i dati
 response = requests.get(url)
@@ -112,7 +141,7 @@ if response.status_code == 200:
 else:
     print(f"Errore nella richiesta: {response.status_code}")
 
-url = home_url + 'matchesbydate/20250402'
+url = home_url + 'matchesbydate/20250331'
 response = requests.get(url)
 
 if response.status_code == 200:
@@ -120,22 +149,22 @@ if response.status_code == 200:
 else:
     print(f"Errore nella richiesta: {response.status_code}")
 
-partite = []
+matches_data = []
 for tournament in matches["tournaments"]:
     if tournament["tournamentId"] in [t["id"] for t in topTournaments["topTournaments"]]:
         for match in tournament["matches"]:
-            partite.append({
-                "Partita": f"{match['homeTeamName']} - {match['awayTeamName']} {match['homeScore']} - {match['awayScore']}",
-                "ID": match["id"]
+            matches_data.append({
+                "homeTeamName": match["homeTeamName"],
+                "awayTeamName": match["awayTeamName"],
+                "homeTeamId": match["homeTeamId"],
+                "awayTeamId": match["awayTeamId"],
+                "match_name": f"{match['homeTeamName']} - {match['awayTeamName']}",
+                "score": f"{match['homeScore']} - {match['awayScore']}" if match["homeScore"] is not None and match["awayScore"] is not None else "In corso",
+                "tournament_name": tournament["tournamentName"],
+                "tournament_id" : tournament["tournamentId"],
+                "id": match["id"]
             })
 
-for partita in partite:
-    if st.button(f"Dettagli {partita['Partita']}"):
-        st.session_state.partita_selezionata = partita["ID"]
-        st.session_state.pagina = "details"
-
-
-# Navbar con scelta del campionato e settimana
 st.sidebar.title("Navigazione")
 
 if not topTournaments == None:
@@ -148,22 +177,28 @@ st.title("Partite di Calcio del giorno")
 #st.write(f"**Campionato**: {campionato}")
 #st.write(f"**Settimana**: {settimana.strftime('%d %B %Y')}")
 
-# Creazione di dati di esempio per le partite
-partite = pd.DataFrame({
-    'Partita': ['Juventus vs Milan', 'Napoli vs Roma', 'Inter vs Lazio'],
-    'Risultato': ['2-1', '1-1', '3-0'],
-    'Data': ['2025-04-01', '2025-04-01', '2025-04-01']
-})
-
 # Mostrare le partite della giornata
-for index, partita in partite.iterrows():
-    with st.expander(f"{partita['Partita']} - {partita['Risultato']}"):
-        st.write(f"**Data**: {partita['Data']}")
-        st.write("Clicca per dettagli")
-        if st.button(f"Dettagli {partita['Partita']}"):
+for match_data in matches_data:
+    with st.expander(f"{match_data['match_name']} - {match_data['score']}"):
+        #st.write(f"**Data**: {partita['Data']}")
+        colHome, colAway = st.columns(2)
+
+        with colHome:
+            st.write(f"**{match_data['homeTeamName']}**")
+            if st.button("Passaggi", key=f"home_pass_{match_data['id']}"):
+                image_creator(home_url + f'match/{match_data["id"]}/team/{match_data["homeTeamId"]}/event/Pass', 'Pass', match_data['homeTeamName'], match_data['awayTeamName'])
+            if st.button("Contrasti", key=f"home_tackle_{match_data['id']}"):
+                image_creator(home_url + f'match/{match_data["id"]}/team/{match_data["homeTeamId"]}/event/Tackle', 'Tackle', match_data['homeTeamName'], match_data['awayTeamName'])
+
+        with colAway:
+            st.write(f"**{match_data['awayTeamName']}**")
+            if st.button("Passaggi", key=f"away_pass_{match_data['id']}"):
+                image_creator(home_url + f'match/{match_data["id"]}/team/{match_data["awayTeamId"]}/event/Pass', 'Pass', match_data['awayTeamName'], match_data['homeTeamName'])
+            if st.button("Contrasti", key=f"away_tackle_{match_data['id']}"):
+                image_creator(home_url + f'match/{match_data["id"]}/team/{match_data["awayTeamId"]}/event/Tackle', 'Tackle', match_data['awayTeamName'], match_data['homeTeamName'])
             # Passaggio alla dashboard della partita
-            st.session_state.partita_selezionata = partita['Partita']
-            st.session_state.risultato = partita['Risultato']
+            #st.session_state.partita_selezionata = match_data['match_name']
+            #st.session_state.risultato = match_data['score']
 
 # Dashboard della partita
 if 'partita_selezionata' in st.session_state and st.session_state.partita_selezionata is not None:
